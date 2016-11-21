43eddfdeb4d0fff0c2be1f17f6a2f06983e10aa6
Qingying Liu <648501626@qq.com>
2016-11-21 22:03:48 +0800

    上传代码
    pod

---
+++ MVVM/MVVM/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MVVM
+//
+//  Created by 营 on 2016/11/21.
+//  Copyright © 2016年 营. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
